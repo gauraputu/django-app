ff2ac61e6534c360717924a6cee366397c2ac463
gaura <gauraputu@gmail.com>
2023-12-28 12:49:24 +0800

    update test & Gitlab CI

--- composeexample/test.py
+++
@@ -1,5 +0,0 @@
-from django.test import TestCase
-
-class simpleTest(TestCase):
-    def test1():
-        print('passed')
---
+++ test.py
@@ -0,0 +1,14 @@
+def test1():
+    #sample test case
+    print('test passed')
+def test2():
+    #sample test case
+    print('test passed')
+def test3():
+    #sample test case
+    print('test passed')
+
+
+test1()
+test2()
+test3()
